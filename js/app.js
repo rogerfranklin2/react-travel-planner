7584d37511a5144fdaca22c9b22569d2f360ee0a
rfr03 <roger.franklin@bskyb.com>
2015-04-20 19:11:19 +0100

    Base set up of app, webpack, index file and simple components

---
+++ js/app.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Router from 'react-router';
+import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
+
+import Homepage from './components/Homepage.js'
+
+let App = React.createClass({
+    render() {
+        return (
+            <div className="travel-planner">
+                <div className="nav">
+                    <Link to="app">Travel planner</Link>
+                </div>
+                <div className="content">
+                    <RouteHandler/>
+                </div>
+            </div>
+        );
+    }
+});
+
+let routes = (
+    <Route name="app" path="/" handler={App}>
+        <DefaultRoute handler={Homepage}/>
+    </Route>
+);
+
+Router.run(routes, function (Handler) {
+    React.render(<Handler/>, document.body);
+});
---
+++ js/components/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+let Homepage = React.createClass({
+
+    render() {
+        return(<div>This is the homepage. There will be a Map on the left and a list on the right maybe</div>);
+    }
+});
+
+export default Homepage;
